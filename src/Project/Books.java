364167bb19728f1b375e425031f2172d59c5a0a4
silvurstein <sveinursilvurstein@gmail.com>
2026-02-20 13:23:29 +0100

    helloq

--- src/Project/Books.java
+++ src/Project/Books.java
@@ -1,7 +1,6 @@
 package Project;
 
 public class Books extends LoanTracker {
-
     public Books(String title, int loan);
     super(title, loan);
 
--- src/Project/LoanTracker.java
+++ src/Project/LoanTracker.java
@@ -4,34 +4,4 @@ public abstract class LoanTracker {
     private String title;
     private int loan;
     private int fine;
-
-    // Constructor
-    public LoanTracker(String title, int loan) {
-        this.title = title;
-        this.loan = loan;
-    }
-
-    // Getter
-    public String getTitle() {
-        return title;
-    }
-
-    public int getLoan() {
-        return loan;
-    }
-
-    // Setter
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setExpiryDays(int loan) {
-        this.loan = loan;
-    }
-
-    @Override
-    public String toString() {
-        return "Title: " + title + " | Loam days: " + loan;
-    }
-
 }
