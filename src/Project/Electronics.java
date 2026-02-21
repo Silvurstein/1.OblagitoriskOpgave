dd671b8ff26916107ef068cb62047f6b2e185cdf
silvurstein <sveinursilvurstein@gmail.com>
2026-02-21 13:24:21 +0100

    Books 1

--- src/Project/Electronics.java
+++ src/Project/Electronics.java
@@ -1,4 +1,11 @@
 package Project;
 
 public class Electronics extends LoanTracker{
+    public Electronics(String title, int loan){
+        super(title, loan);
+
+    }
+    public void ChooseBook(){
+        system
+    }
 }
