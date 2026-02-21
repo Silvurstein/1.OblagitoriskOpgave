88d95b8345c9816e4a5403e060a809a211416d89
FrankHamp <valdemarengelund@gmail.com>
2026-02-21 13:38:51 +0100

    2

--- src/Project/Electronics.java
+++ src/Project/Electronics.java
@@ -3,18 +3,23 @@ package Project;
 public class Electronics extends LoanTracker {
 
     String model;
+    int baseValue;
+    String kitLevel;
 
     public Electronics(String title, int expiryDays, int loan, String model, int baseValue, String kitLevel) {
         super(title, expiryDays, loan, model, baseValue, kitLevel);
+        this.model = model;
+        this.baseValue = baseValue;
+        this.kitLevel = kitLevel;
     }
 
-    public void rentItem() {
+    public void lendItem() {
         System.out.println(getTitle() + " item rented ");
     }
 
     @Override
-    public void useItem() {
-        rentItem();
+    public void displayInfo() {
+        lendItem();
     }
 
     @Override
